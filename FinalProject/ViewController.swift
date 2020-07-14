f675abd5b233dab83795902513326f736b381fb5
solongot <shanelelfe@gmail.com>
2020-07-14 13:36:13 -0400

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Shanel on 7/14/20.
+//  Copyright © 2020 Solongo Tumenhuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
